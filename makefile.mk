0f5c7019ded6aa8862b93f08d72c2d828f6acf8c
inkeliz <inkeliz@inkeliz.com>
2024-06-03 11:02:09 +0100

    all: add Makefile for demo and generator
    Signed-off-by: inkeliz <inkeliz@inkeliz.com>

---
+++ makefile.mk
@@ -0,0 +1,28 @@
+GO = "go"
+GOGIO = "gioui.org/cmd/gogio"
+ANDROID_SDK_ROOT = ""
+ANDROID_JAVA_ROOT = ""
+TEMP = ""
+
+ifeq ($(shell uname -s), Darwin)
+    ANDROID_SDK_ROOT = "$(HOME)/Library/Android/sdk/"
+    ANDROID_JAVA_ROOT = /Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home/bin
+    ANDROID_PLATFORM = $(ANDROID_SDK_ROOT)/platforms/$(shell ls $(ANDROID_SDK_ROOT)/platforms | sort -n | tail -n 1)
+    TEMP = /tmp
+endif
+
+define generate_java
+java_$(1):
+	mkdir -p $(TEMP)/$(1)_android/classes
+	PATH=$(ANDROID_JAVA_ROOT):$(PATH) javac -source 8 -target 8 -bootclasspath $(ANDROID_PLATFORM)/android.jar -d $(TEMP)/$(1)_android/classes $(1)_android.java
+	jar cf $(1)_android.jar -C $(TEMP)/$(1)_android/classes .
+	rm -rf $(TEMP)/$(1)_android
+endef
+
+define generate_inkwasm
+inkwasm_$(1):
+	$(GO) run github.com/inkeliz/go_inkwasm@master generate .
+	mv inkwasm_js.go $(1)_js_wasm.go
+	mv inkwasm_js.s $(1)_js_wasm.s
+	mv inkwasm_js.js $(1)_js.js
+endef
