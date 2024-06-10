725cd5dbeeb35c3e624a500f4413888f23965d94
inkeliz <inkeliz@inkeliz.com>
2024-06-10 18:03:36 +0100

    doc: [hyperlink] fix documentation
    Signed-off-by: inkeliz <inkeliz@inkeliz.com>

--- hyperlink/config_js.go
+++ hyperlink/config_js.go
@@ -4,5 +4,6 @@ package hyperlink
 
 // Config is the configuration for a Hyperlink.
 type Config struct {
+	// Blur must be true if the current window is not in focus.
 	Blur bool
 }
--- hyperlink/giohyperlink/config.go
+++ hyperlink/giohyperlink/config.go
@@ -7,13 +7,15 @@ import (
 	"github.com/gioui-plugins/gio-plugins/hyperlink"
 )
 
-// NewConfigFromViewEvent creates a share.Config based on app.ViewEvent.
+// NewConfigFromViewEvent creates a hyperlink.Config based on app.ViewEvent.
 func NewConfigFromViewEvent(w *app.Window, e app.ViewEvent) hyperlink.Config {
 	r := hyperlink.Config{}
 	UpdateConfigFromViewEvent(&r, w, e)
 	return r
 }
 
+// UpdateConfigFromViewEvent updates a hyperlink.Config based on app.ViewEvent.
 func UpdateConfigFromViewEvent(config *hyperlink.Config, w *app.Window, e app.ViewEvent) {}
 
+// UpdateConfigFromConfigEvent creates a hyperlink.Config based on app.ConfigEvent.
 func UpdateConfigFromConfigEvent(config *hyperlink.Config, _ *app.Window, e app.ConfigEvent) {}
